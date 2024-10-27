fbacf9ecd6973f6c7623b22dc438c592394e5be7
Matthew Deeprose <92100175+matthewdeeprose@users.noreply.github.com>
2024-10-27 19:37:17 +0000

    updates

--- suggester/app.js
+++ suggester/app.js
@@ -1,9 +1,15 @@
+
 import { ColorChecker } from './colorChecker.js';
 
+console.log('App.js is loading...');
+
 document.addEventListener('DOMContentLoaded', async () => {
+    console.log('DOM Content Loaded...');
     try {
         const colorChecker = new ColorChecker();
         await colorChecker.init();
+        colorChecker.initRandomizeButton();
+        console.log('ColorChecker initialized successfully');
     } catch (error) {
         console.error('Failed to initialize ColorChecker:', error);
     }
